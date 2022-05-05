5e4eb9c1cfadb2042d4eced0f753f460a3a9da41
Damian1255 <damianlau96@gmail.com>
2022-05-05 15:27:43 +0800

    navbar

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,6 @@
 function displayNav() {
   var x = document.getElementById("myTopnav");
+
   if (!x.className.includes("responsive")) {
     x.classList.add("responsive");
   } else {
@@ -8,11 +9,18 @@ function displayNav() {
 }
 
 var navbar = document.getElementById("myTopnav");
+var logo = document.getElementById("navLogo");
+var account = document.getElementById("account");
+
 window.onscroll = function() {
   if (window.pageYOffset >= 300) {
     navbar.classList.add("sticky");
+    logo.src = "resources/images/logov2_black.png";
+    account.src = "resources/images/user_black.png";
   }
   else {
-      navbar.classList.remove("sticky");
-    }
+    navbar.classList.remove("sticky");
+    logo.src = "resources/images/logov2_white.png";
+    account.src = "resources/images/user_white.png";
+  }
 }
