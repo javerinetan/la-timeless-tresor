36986d05b12c8956abd4858bff66323dd3f5f185
Damian1255 <damianlau96@gmail.com>
2022-05-07 15:28:41 +0800

    sidebar

--- js/index.js
+++ js/index.js
@@ -10,17 +10,36 @@ function displayNav() {
 
 var navbar = document.getElementById("myTopnav");
 var logo = document.getElementById("navLogo");
+var sidebtn = document.getElementById("sideBtn");
 var account = document.getElementById("account");
+var form = document.getElementById("bookForm");
 
 window.onscroll = function() {
   if (window.pageYOffset >= 300) {
     navbar.classList.add("sticky");
+    sidebtn.classList.add("alt-color");
+    form.classList.add("alt-color");
+
     logo.src = "resources/images/logov2_black.png";
     account.src = "resources/images/user_black.png";
   }
   else {
     navbar.classList.remove("sticky");
+    sidebtn.classList.remove("alt-color");
+    form.classList.add("alt-color");
+
     logo.src = "resources/images/logov2_white.png";
     account.src = "resources/images/user_white.png";
+  }
+}
+
+
+function displayBookingForm() {
+  if (!form.className.includes("show-form")) {
+    form.classList.add("show-form");
+    sidebtn.classList.add("btn-active");
+  } else {
+    form.classList.remove("show-form");
+    sidebtn.classList.remove("btn-active");
   }
 }
--- js/nav.js
+++ js/nav.js
@@ -9,17 +9,23 @@ function displayNav() {
 }
 
 var navbar = document.getElementById("myTopnav");
+var sidebtn = document.getElementById("sideBtn");
+
 var logo = document.getElementById("navLogo");
 var account = document.getElementById("account");
 
 window.onscroll = function() {
   if (window.pageYOffset >= 300) {
     navbar.classList.add("sticky");
+    sidebtn.classList.add("alt-color");
+
     logo.src = "../resources/images/logov2_black.png";
     account.src = "../resources/images/user_black.png";
   }
   else {
     navbar.classList.remove("sticky");
+    sidebtn.classList.remove("alt-color");
+
     logo.src = "../resources/images/logov2_white.png";
     account.src = "../resources/images/user_white.png";
   }
