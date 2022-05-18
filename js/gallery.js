8d1a4a92a23b717e7abf5f4e73a010b7128e6970
Neo Ru En Natelie <neo_ru_en_natelie@connect.ite.edu.sg>
2022-05-18 22:48:03 +0800

    abc

---
+++ js/gallery.js
@@ -0,0 +1,9 @@
+function show(section) {
+  document.getElementById("all").className="all"
+  document.getElementById("exterior").className="exterior"
+  document.getElementById("dining").className="dining"
+  document.getElementById("experience").className="experience"
+  document.getElementById("interior").className="interior"
+  console.log("wenhui <3 raihan")
+  document.getElementById(section).classList.add("display")
+}
