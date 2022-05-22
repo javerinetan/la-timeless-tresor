9263d1dfbe198fa38b07d13f654e8ed8532bedd0
Damian12555 <damianlau96@gmail.com>
2022-05-22 20:13:42 +0800

    hellooooooo

--- js/index.js
+++ js/index.js
@@ -71,3 +71,30 @@ function scrollAction(){
     window.scrollTo(0, window.innerHeight - navbar.clientHeight);
   }
 }
+
+// Carousel
+var carousel = document.querySelector('.carousel');
+var flkty = new Flickity( carousel, {
+  imagesLoaded: true,
+  percentPosition: false,
+  freeScroll: true,
+  contain: true,
+  prevNextButtons: false,
+  pageDots: false,
+  autoPlay: 1500,
+  pauseAutoPlayOnHover: false
+});
+
+var imgs = carousel.querySelectorAll('.carousel-cell img');
+// get transform property
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
+  'transform' : 'WebkitTransform';
+
+flkty.on( 'scroll', function() {
+  flkty.slides.forEach( function( slide, i ) {
+    var img = imgs[i];
+    var x = ( slide.target + flkty.x ) * -1/3;
+    img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+  });
+});
