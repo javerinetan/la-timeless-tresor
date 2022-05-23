8bb9ff978ea451e9512ebdf71cea8f3c82618480
Damian1255 <damianlau96@gmail.com>
2022-05-23 22:51:27 +0800

    animations

--- js/index.js
+++ js/index.js
@@ -72,6 +72,22 @@ function scrollAction(){
   }
 }
 
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("animate");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
 // Carousel
 var carousel = document.querySelector('.carousel');
 var flkty = new Flickity( carousel, {
