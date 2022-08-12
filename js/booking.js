82b7ebc1144db919830e0d515be35dd63db715d3
Damian1255 <damianlau96@gmail.com>
2022-08-13 01:06:40 +0800

    page changes

--- js/booking.js
+++ js/booking.js
@@ -24,11 +24,34 @@ function displayResults() {
 
 // updates booking details
 function updateBooking(element) {
-  document.getElementById(element.name).innerHTML = document.getElementById(element.id).value;
+  document.getElementById(element.name).value = document.getElementById(element.id).value;
 }
 
 // update room type in booking details
 function updateRoomType(name, id) {
   console.log(id);
-  document.getElementById(id).innerHTML = name;
+  document.getElementById(id).value = name;
 }
+
+//this is to get the current date time
+//split them to variable
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+//make sure that date is correct
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+//set the check in and check out date
+var dateTomorrow = year + "-" + month + "-" + date;
+var checkinElem = document.querySelector("#chkin");
+var checkoutElem = document.querySelector("#chkout");
+
+checkinElem.setAttribute("min", dateTomorrow);
+checkoutElem.setAttribute("min", dateTomorrow);
