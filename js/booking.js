3004682e9ee59dc896e6d03f2e7a146048598fd1
Damian12555 <daminlau125@gmail.com>
2022-08-12 11:26:08 +0800

    some changes

--- js/booking.js
+++ js/booking.js
@@ -3,6 +3,7 @@ var form = document.getElementById("form-section");
 
 window.addEventListener('scroll', colorChange);
 
+// changes booking detail form color
 function colorChange(){
   // console.log(window.pageYOffset);
   // console.log(window.innerHeight + form.clientHeight);
@@ -14,16 +15,19 @@ function colorChange(){
   }
 }
 
+// show search result
 function displayResults() {
   document.getElementById("results-title").innerHTML = "We found 2 Available Rooms";
   document.getElementById("results-sub").style.display = "none";
   document.getElementById("items").style.display = "block";
 }
 
+// updates booking details
 function updateBooking(element) {
   document.getElementById(element.name).innerHTML = document.getElementById(element.id).value;
 }
 
+// update room type in booking details
 function updateRoomType(name, id) {
   console.log(id);
   document.getElementById(id).innerHTML = name;
