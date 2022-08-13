0a73014595edada0def80408fb466e49e8dd6cee
Damian1255 <damianlau96@gmail.com>
2022-08-13 12:49:28 +0800

    renamed the f&b page to orginal

--- js/FoodandBrev.js
+++ js/FoodandBrev.js
@@ -6,11 +6,15 @@ var flkty = new Flickity( review, {
 });
 
 function displayMenu(element){
+  var menus = ["maincourses", "entrees","desserts", "drinks"];
+  for (var menu of menus) {
+    document.getElementById(menu).style.display = "none";
+  }
   boxes = document.querySelectorAll('.Course li');
   for (const box of boxes) {
     box.classList.remove('selected');
   }
 
   element.classList.add('selected');
-
+  document.getElementById(element.title).style.display = "block";
 }
