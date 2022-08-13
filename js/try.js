c415e20147471cddb681b0e5425fcf5cba850c85
amberyeoe <104883642+amberyeoe@users.noreply.github.com>
2022-08-13 15:55:54 +0800

    pls look at pool booking webpage and look at the forms if u guys ok with it !!

--- js/try.js
+++ js/try.js
@@ -1,35 +1,3 @@
-//DATEEE
-function date(){
-//this is to get the current date time
-//split them to variable
-  var currentDateTime = new Date();
-  var year = currentDateTime.getFullYear();
-  var month = (currentDateTime.getMonth() + 1);
-  var date = (currentDateTime.getDate() + 1);
-
-  //make sure that date is correct
-  if(date < 10) {
-    date = '0' + date;
-  }
-  if(month < 10) {
-    month = '0' + month;
-  }
-
-  //set the check in and check out date
-  var dateTomorrow = year + "-" + month + "-" + date;
-  var checkinElem = document.querySelector("#checkin-date");
-  var checkoutElem = document.querySelector("#checkout-date");
-  var reservationdate = document.querySelector("#date");
-
-  reservationdate.setAttribute("min",dateTomorrow) //this is so it starts one day after the day it is today
-  checkinElem.setAttribute("min", dateTomorrow);
-
-  checkinElem.onchange = function () {
-      checkoutElem.setAttribute("min", this.value);
-  }
-}
-//forms
-
 var create_workspace=document.querySelector("#next");
 var next_click=document.querySelectorAll(".next-click");
 var back_click=document.querySelectorAll(".back-click");
@@ -72,8 +40,12 @@ finish_click.addEventListener('click',function(){
 //         }
          formnumber++;
          updateform();
-         var remove_progress=document.querySelector(".progress-bar");
+         var remove_progress=document.querySelector(".progressbar");
          remove_progress.classList.add('d-none');
+         var title= document.querySelector(".section-title");
+         title.innerHTML="Your Reservation has been confirmed";
+         var status=document.querySelector(".status");
+         status.style.marginBottom="20px";
 });
 
 
@@ -100,14 +72,11 @@ function updateform(){
 
 function validateform(){
     let validate=true; //overall
-    // let inputValidate=true;
-    // let selectValidate=true;
     let inputCounter=0;
     let selectCounter=0;
     var validate_form=document.querySelectorAll(".page.currently input");
     validate_form.forEach(function(val){
-        val.classList.remove('warning');
-        if(val.hasAttribute('require')){
+        if(val.hasAttribute('required')){
             if(val.value.length==0){
                 inputCounter++;
             }
@@ -123,8 +92,31 @@ function validateform(){
     let sum= inputCounter + selectCounter;
     if (sum > 0){
       validate=false;
-      alert("Please fill in all the fields")
+      alert("Please fill in all the field(s)")
     }
 
     return validate;
 }
+
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+//make sure that date is correct
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+//set the check in and check out date
+var dateTomorrow = year + "-" + month + "-" + date;
+
+var reservationdate = document.querySelectorAll("#date");
+
+reservationdate.forEach(function(date){
+  date.setAttribute("min",dateTomorrow) //this is so it starts one day after the day it is today
+
+})
