1553830b3b2798272f6c0f982558777248449c91
jae <90853880+jaestayateeznct@users.noreply.github.com>
2022-08-13 16:55:44 +0800

    js

--- js/form.js
+++ js/form.js
@@ -1,3 +1,104 @@
+var create_workspace=document.querySelector("#next");
+var next_click=document.querySelectorAll(".next-click");
+var back_click=document.querySelectorAll(".back-click");
+var finish_click=document.querySelector(".finish-click");
+var page_form=document.querySelectorAll(".page");
+var list=document.querySelectorAll(".progress-bar li")
+var progress = document.getElementById("progress");
+var progressSteps = document.querySelectorAll(".progress-step");
+let formnumber=0;
+
+create_workspace.addEventListener('click',function(){
+    if(!validateform()){
+        return false;
+    }
+   formnumber++;
+   updateform();
+});
+
+next_click.forEach(function(next_page){
+    next_page.addEventListener('click',function(){
+         if(!validateform()){
+        return false;
+        }
+         formnumber++;
+         updateform();
+    });
+});
+
+
+back_click.forEach(function(back_page){
+    back_page.addEventListener('click',function(){
+         formnumber--;
+         updateform();
+    });
+});
+
+finish_click.addEventListener('click',function(){
+//   if(!validateform()){
+//         return false;
+//         }
+         formnumber++;
+         updateform();
+         var remove_progress=document.querySelector(".progressbar");
+         remove_progress.classList.add('d-none');
+         var title= document.querySelector(".section-title");
+         title.innerHTML="Your Reservation has been confirmed";
+         var status=document.querySelector(".status");
+         status.style.marginBottom="20px";
+});
+
+
+function updateform(){
+    page_form.forEach(function(page_number){
+       page_number.classList.remove('currently'); //reset the page content
+    });
+    page_form[formnumber].classList.add('currently');
+
+    //reset the colored progress circles
+    progressSteps.forEach((progressStep, idx) => {
+      if (idx < formnumber + 1) {
+        progressStep.classList.add("progress-step-active");
+      } else {
+        progressStep.classList.remove("progress-step-active");
+      }
+    });
+    //change the progress bar colour
+    const progressActive = document.querySelectorAll(".progress-step-active");
+
+    progress.style.width =
+      ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+}
+
+function validateform(){
+    let validate=true; //overall
+    let inputCounter=0;
+    let selectCounter=0;
+    var validate_form=document.querySelectorAll(".page.currently input");
+    validate_form.forEach(function(val){
+        if(val.hasAttribute('required')){
+            if(val.value.length==0){
+                inputCounter++;
+            }
+        }
+    });
+
+    var slot=document.querySelectorAll(".page.currently select");
+    slot.forEach(function(selection){
+      if (selection.value==""){
+        selectCounter++;
+      }
+    })
+    let sum= inputCounter + selectCounter;
+    if (sum > 0){
+      validate=false;
+      alert("Please fill in all the field(s)")
+    }
+
+    return validate;
+}
+
+
 //this is to get the current date time
 //split them to variable
 var currentDateTime = new Date();
