2a158fa2f879d894dcea131a40b35c9080549100
amberyeoe <104883642+amberyeoe@users.noreply.github.com>
2022-08-13 11:38:21 +0800

    trying to do the progress bar for the form

---
+++ js/try.js
@@ -0,0 +1,131 @@
+//DATEEE
+function date(){
+//this is to get the current date time
+//split them to variable
+  var currentDateTime = new Date();
+  var year = currentDateTime.getFullYear();
+  var month = (currentDateTime.getMonth() + 1);
+  var date = (currentDateTime.getDate() + 1);
+
+  //make sure that date is correct
+  if(date < 10) {
+    date = '0' + date;
+  }
+  if(month < 10) {
+    month = '0' + month;
+  }
+
+  //set the check in and check out date
+  var dateTomorrow = year + "-" + month + "-" + date;
+  var checkinElem = document.querySelector("#checkin-date");
+  var checkoutElem = document.querySelector("#checkout-date");
+  var reservationdate = document.querySelector("#date");
+
+  reservationdate.setAttribute("min",dateTomorrow) //this is so it starts one day after the day it is today
+  checkinElem.setAttribute("min", dateTomorrow);
+
+  checkinElem.onchange = function () {
+      checkoutElem.setAttribute("min", this.value);
+  }
+}
+//forms
+
+var create_workspace=document.querySelector("#next");
+var next_click=document.querySelectorAll(".next-click");
+var back_click=document.querySelectorAll(".back-click");
+var finish_click=document.querySelector(".finish-click");
+var page_form=document.querySelectorAll(".page");
+var list=document.querySelectorAll(".progress-bar li")
+var progress = document.getElementById("progress");
+var progressSteps = document.querySelectorAll(".progress-step");
+let formnumber=0;
+
+create_workspace.addEventListener('click',function(){
+    if(!validateform()){
+        return false;
+    }
+   formnumber++;
+   updateform();
+});
+
+next_click.forEach(function(next_page){
+    next_page.addEventListener('click',function(){
+         if(!validateform()){
+        return false;
+        }
+         formnumber++;
+         updateform();
+    });
+});
+
+
+back_click.forEach(function(back_page){
+    back_page.addEventListener('click',function(){
+         formnumber--;
+         updateform();
+    });
+});
+
+finish_click.addEventListener('click',function(){
+//   if(!validateform()){
+//         return false;
+//         }
+         formnumber++;
+         updateform();
+         var remove_progress=document.querySelector(".progress-bar");
+         remove_progress.classList.add('d-none');
+});
+
+
+function updateform(){
+    page_form.forEach(function(page_number){
+       page_number.classList.remove('currently'); //reset the page content
+    });
+    page_form[formnumber].classList.add('currently');
+
+    //reset the colored progress circles
+    progressSteps.forEach((progressStep, idx) => {
+      if (idx < formnumber + 1) {
+        progressStep.classList.add("progress-step-active");
+      } else {
+        progressStep.classList.remove("progress-step-active");
+      }
+    });
+    //change the progress bar colour
+    const progressActive = document.querySelectorAll(".progress-step-active");
+
+    progress.style.width =
+      ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+}
+
+function validateform(){
+    date();
+    let validate=true; //overall
+    // let inputValidate=true;
+    // let selectValidate=true;
+    let inputCounter=0;
+    let selectCounter=0;
+    var validate_form=document.querySelectorAll(".page.currently input");
+    validate_form.forEach(function(val){
+        val.classList.remove('warning');
+        if(val.hasAttribute('require')){
+            if(val.value.length==0){
+                inputCounter++;
+            }
+        }
+    });
+
+    var slot=document.querySelectorAll(".page.currently select");
+    slot.forEach(function(selection){
+      if (selection.value==""){
+        selectCounter++;
+      }
+    })
+    let sum= inputCounter + selectCounter;
+    if (sum > 0){
+      validate=false;
+      alert("Please fill in all the fields")
+    }
+
+    return validate;
+}
